c0757d4a7cd5f3cc5c3f91af19205d34509e9fb0
Tuan Nguyen <tuan@atndev.com>
2023-03-21 14:51:21 -0500

    update screens

--- GettingStarted/forms/orders.js
+++ GettingStarted/forms/orders.js
@@ -12,56 +12,29 @@ var searchTerm = '';
  */
 var totalSales = 0;
 /**
- * Callback method for when form is shown.
- *
- * @param {Boolean} firstShow form is shown first time after load
- * @param {JSEvent} event the event that triggered the action
+ * @param {JSEvent} event
  *
  * @private
  *
- * @properties={typeid:24,uuid:"6D07D285-0E4B-431A-9B07-BC28A6E682D6"}
+ * @properties={typeid:24,uuid:"81E851DE-02B2-42E9-B441-E40C3605BE8F"}
  */
-function onShow(firstShow, event) {
-//	totalSales = 0;
-//	var chart = elements.chart_10;
-//	chart.setOptions({
-//		legend: false
-//	})
-//	var chart_data = {
-//		type: 'bar',
-//		data: {
-//			labels: [],
-//			datasets: [{
-//				data: [],
-//				backgroundColor: []
-//			}]
-//		}
-//	}
-//
-//	var q = datasources.db.example_data.orders.createSelect();
-//	q.groupBy.add(q.columns.customerid)
-//	q.result.add(q.columns.customerid).add(q.joins.orders_to_order_details.columns.unitprice.sum)
-//	q.result.distinct = true;
-//	q.sort.add(q.joins.orders_to_order_details.columns.unitprice.sum.desc)
-//
-//	var ds = databaseManager.getDataSetByQuery(q, -1);
-//	for (var i = 1; i <= ds.getMaxRowIndex(); i++) {
-//		if (i < 16) { //get me the top 15 customers
-//			var row = ds.getRowAsArray(i);
-//			if (row[0]) {
-//				//setting up the chart
-//				chart_data.data.labels.push(row[0])
-//				chart_data.data.datasets[0].data.push(row[1])
-//				chart_data.data.datasets[0].backgroundColor.push(scopes.globals.getUniqueColor())
-//			}
-//			//tallying up the total sales
-//			totalSales += row[1] 
-//		}
-//		
-//	}
-//	//send data to chart to draw
-//	chart.setData(chart_data);
+function onAction$newOrder(event) {
+	foundset.newRecord();
+	foundset.orderdate = new Date();	
 }
+
+
+/**
+ * @param {JSEvent} event
+ *
+ * @private
+ *
+ * @properties={typeid:24,uuid:"C242D11C-7E91-4DDB-BAD3-DE56D7FE6334"}
+ */
+function onAction$addDetail(event) {	
+	foundset.orders_to_order_details.newRecord();	
+}
+
 /**
  * Called when the columns data is changed.
  *
@@ -85,33 +58,9 @@ function onColumnDataChange(foundsetindex, columnindex, oldvalue, newvalue, even
 		var curr_rec = foundset.orders_to_order_details.getSelectedRecord();
 		curr_rec.unitprice = curr_rec.order_details_to_products.unitprice;
 	}
-	//need to recalculate otherwise chart doesn't show up properly
-	databaseManager.recalculate(foundset.orders_to_order_details)
 	return true;
 }
 
-/**
- * @param {JSEvent} event
- *
- * @private
- *
- * @properties={typeid:24,uuid:"C242D11C-7E91-4DDB-BAD3-DE56D7FE6334"}
- */
-function onAction(event) {	
-	foundset.orders_to_order_details.newRecord();	
-}
-
-/**
- * @param {JSEvent} event
- *
- * @private
- *
- * @properties={typeid:24,uuid:"81E851DE-02B2-42E9-B441-E40C3605BE8F"}
- */
-function onAction$newOrder(event) {
-	foundset.newRecord();
-	foundset.orderdate = new Date();	
-}
 
 /**
  * Handle changed data, return false if the value should not be accepted.
@@ -129,8 +78,10 @@ function onAction$newOrder(event) {
  * @AllowToRunInFind
  */
 function onDataChange(oldValue, newValue, event) {
-	foundset.find()
-	foundset.orders_to_customers.companyname = '#%'+searchTerm+'%';	
-	foundset.search();
-	return true
+	var s = scopes.svySearch.createSimpleSearch(foundset);
+	s.addSearchProvider('orders_to_customers.companyname');
+	s.addSearchProvider('orders_to_order_details.order_details_to_products.productname')
+	s.setSearchText(searchTerm);
+	s.loadRecords(foundset);
+	return true;
 }
